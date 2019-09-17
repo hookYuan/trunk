ff976489e7bbe09bfa0d5fe8f7ebf1bf9c20a625
yuanye <yy962851730@126.com>
2019-09-17 14:36:38 +0800

    取消吸顶装饰器

--- app/src/main/java/com/yuan/simple/core/ui/adapter/MultipleActivity.java
+++ app/src/main/java/com/yuan/simple/core/ui/adapter/MultipleActivity.java
@@ -1,9 +1,5 @@
 package com.yuan.simple.core.ui.adapter;
 
-import android.view.LayoutInflater;
-import android.view.View;
-
-import com.yuan.simple.R;
 import com.yuan.simple.core.adapter.MultiTypeAdapter;
 import com.yuan.simple.core.module.SubjectBean;
 import com.yuan.simple.core.presenter.AdapterPresenter;
@@ -14,7 +10,6 @@ import java.util.List;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import yuan.core.list.GridDivider;
-import yuan.core.list.StickyHelper;
 import yuan.core.title.TitleBar;
 import yuan.core.ui.Adapter;
 import yuan.core.ui.RecyclerActivity;
@@ -43,14 +38,6 @@ public class MultipleActivity extends RecyclerActivity<AdapterPresenter, Subject
     @Override
     public void initData() {
         mRecyclerView.addItemDecoration(new GridDivider());
-        mRecyclerView.addItemDecoration(new StickyHelper(new StickyHelper.StickyCallback() {
-            @Override
-            public View getStickyView() {
-                View view = LayoutInflater.from(mContext)
-                        .inflate(R.layout.simple_item2, mRecyclerView, false);
-                return view;
-            }
-        }));
 
         getPresenter().loadData(mData);
         List<String> menu = new ArrayList<>();
